d5363532326455bddd49a7238f40db0eb1a4eba9
Manissara Saejan <sirapopgolf2548@gmail.com>
2026-01-27 15:59:13 +0700

    add

--- Lab/app/src/main/java/com/example/a175lablearnandorid/MainActivity2.kt
+++ Lab/app/src/main/java/com/example/a175lablearnandorid/MainActivity2.kt
@@ -4,11 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.a175lablearnandorid.ui.theme._175LabLearnAndoridTheme
@@ -21,7 +28,7 @@ class MainActivity2 : ComponentActivity() {
             _175LabLearnAndoridTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting2(
-                        name = "Android",
+                        name = "Android2",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -32,16 +39,25 @@ class MainActivity2 : ComponentActivity() {
 
 @Composable
 fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    var inputText by remember { mutableStateOf("") }
+
+    Column {
+        Text(
+            text = "Hello $name! say = "+inputText,
+            modifier = modifier
+        )
+        TextField(
+            value = inputText,
+            onValueChange = { inputText = it }
+        )
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview2() {
     _175LabLearnAndoridTheme {
-        Greeting2("Android")
+        Greeting2("Android2")
     }
 }
